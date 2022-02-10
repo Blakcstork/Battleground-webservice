ae0e2657582710c3875c2ae235f19fab19e99423
이종상 <helius96@naver.com>
2022-02-10 21:32:30 +0900

    Springboot Studying...

--- src/main/java/com/battleground/springboot/service/posts/PostsService.java
+++ src/main/java/com/battleground/springboot/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package com.battleground.springboot.service.posts;
 
 import com.battleground.springboot.domain.posts.Posts;
+import com.battleground.springboot.web.dto.PostsListResponseDto;
 import com.battleground.springboot.web.dto.PostsResponseDto;
 import com.battleground.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.battleground.springboot.domain.posts.PostsRepository;
 import com.battleground.springboot.web.dto.PostsSaveRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,5 +39,10 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 업슴 id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
--- src/main/java/com/battleground/springboot/web/IndexController.java
+++ src/main/java/com/battleground/springboot/web/IndexController.java
@@ -1,15 +1,24 @@
 package com.battleground.springboot.web;
 
+import com.battleground.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
 
     @GetMapping("/posts/save")
     public String postsSave(){
